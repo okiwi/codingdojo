b162dbec21bac1c52c259ea6a66aec6862385d03
Bastien Gallay <bastien@upwiser.com>
2019-09-30 13:57:43 +0200

    Coding Dojo en FSharp sur le FizzBuzz durant le Geek Camp été indien 2019

---
+++ FizzBuzz/FSharp/FSharpFizzBuzz/FizzBuzz/FizzBuzz.fs
@@ -0,0 +1,22 @@
+﻿namespace FizzBuzz
+open System
+
+module FizzBuzz =
+    let say (number: int) =
+        let applicableRules = [(3, "Fizz"); (5, "Buzz")]
+        
+        let applyRule (ruly, result) =
+            match ruly with
+            | true -> result
+            | false -> ""
+            
+        let rec sayWithRules (rules, num, ruleWasApplied) =
+            match (rules, num, ruleWasApplied) with
+            | ([], num, false) -> string num
+            | ([], _, true) -> String.Empty
+            | ((denominator, result) :: tail, num, _) ->
+                let ruly = (number%denominator = 0)
+                applyRule(ruly, result)
+                + sayWithRules(tail, num, ruly || ruleWasApplied)
+        
+        sayWithRules(applicableRules, number, false)
---
+++ FizzBuzz/FSharp/FSharpFizzBuzz/UnitTestFSharp/FizzBuzzTest.fs
@@ -0,0 +1,25 @@
+﻿namespace UnitTestFSharp
+
+open NUnit.Framework
+open FizzBuzz.FizzBuzz
+
+module Say =
+    let [<Test>] ``say 1 returns '1'``() =
+       let expected = "1"
+       Assert.AreEqual(expected, FizzBuzz.FizzBuzz.say (1))
+
+    let [<Test>] ``say 3 returns 'Fizz'``() =
+        let expected = "Fizz"
+        Assert.AreEqual(expected, FizzBuzz.FizzBuzz.say (3))
+
+    let [<Test>] ``say 6 returns 'Fizz'``() =
+        let expected = "Fizz"
+        Assert.AreEqual(expected, FizzBuzz.FizzBuzz.say (6))
+        
+    let [<Test>] ``say 5 returns 'Buzz'``() =
+        let expected = "Buzz"
+        Assert.AreEqual(expected, FizzBuzz.FizzBuzz.say (5))
+    
+    let [<Test>] ``say 15 returns 'FizzBuzz'``() =
+        let expected = "FizzBuzz"
+        Assert.AreEqual(expected, FizzBuzz.FizzBuzz.say (15))
